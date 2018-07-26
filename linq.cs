675d3994db9fbda86aa3a1bbcc97fc5df0f2b293
Igal Greenberg <igalgre@gmail.com>
2018-07-26 10:27:30 +0100

    Create linq.cs
    Linq can also bring comprehensions to c#

---
+++ linq.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter1 {
+    public class Program {
+        static void Main () {
+            IEnumerable<int> numbers = Enumerable.Range (0, 10);
+            var evens = from num in numbers where num % 2 == 0 select num;
+            foreach (var e in evens){
+                Console.WriteLine(e);
+            } 
+        }
+    }
+}
